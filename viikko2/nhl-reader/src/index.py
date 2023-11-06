cff022e389a7d3973d759d33efc4ac7042c32b05
Venla R <venla.e.rantanen@gmail.com>
2023-11-06 15:56:01 +0200

    Tehtävä 2.12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -2,20 +2,31 @@ import requests
 import operator
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.players = self.get_players()
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = [Player(player_dict) for player_dict in response]
+        return players
+            
+class PlayerStats: 
+    def __init__(self, players: PlayerReader):
+        self.playerList = players.players
+        
+    def top_scorers_by_nationality(self, nationality):
+        nat_list = [player for player in self.playerList if player.nationality == nationality]
+        return sorted(nat_list, key=operator.attrgetter("total"), reverse=True)
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        if player.nationality == "FIN":
-            players.append(player)
-    new_players = sorted(players, key=operator.attrgetter("total"), reverse=True)
-            
-    print("Players from FIN")
-    for player in new_players:
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
