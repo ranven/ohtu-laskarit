71927d9c8368ecf96f8e5bcf327f2acdbee52aee
Venla R <venla.e.rantanen@gmail.com>
2023-11-14 14:03:16 +0200

    Tehtävä 7

--- viikko3/web-login/src/services/user_service.py
+++ viikko3/web-login/src/services/user_service.py
@@ -1,4 +1,5 @@
 from entities.user import User
+import re
 from repositories.user_repository import (
     user_repository as default_user_repository
 )
@@ -40,7 +41,26 @@ class UserService:
         if not username or not password:
             raise UserInputError("Username and password are required")
 
-        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if not password_confirmation:
+            raise UserInputError("Password confirmation is required.")
+
+        # username validation
+        if not re.match("^[a-z]{3,}$", username):
+            raise UserInputError(
+                "Invalid username. Username must be at least 3 characters long and consist of letters a-z")
+
+        existing = self._user_repository.find_by_username(username)
+        if existing:
+            raise UserInputError("Username is already taken")
+
+        # password validation
+        if password.isalpha() or len(password) < 8:
+            raise UserInputError(
+                "Invalid password. Password must be at least 8 characters long and not consist of only letters.")
+
+        if password != password_confirmation:
+            raise UserInputError(
+                "Password confirmation didn't match the password.")
 
 
 user_service = UserService()
